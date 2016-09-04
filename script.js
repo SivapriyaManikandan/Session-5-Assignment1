b9772d6cd209a34934388e95d44c738aafd3a200
SivapriyaManikandan <way2priyamail@gmail.com>
2016-09-04 20:44:35 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var name = "Sivapriya";
+var age = 28;
+var dob = "20.10.1987";
+var pob = "Trichy, Tamilnadu";
+alert("This is sample pgm");
+console.log(name);
+console.log(age);
+console.log(dob);
+console.log(pob);
